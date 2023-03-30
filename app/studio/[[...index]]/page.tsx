a4f6bf0f35bd11f265a52461c8ea7d7e46eed818
Dan Perkins <danjonperkins@gmail.com>
2023-03-30 21:41:33 +0100

    fix: sanity nextstudiohead

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -4,9 +4,6 @@ import { NextStudio } from 'next-sanity/studio'
 
 import config from '../../../sanity.config'
 
-// Re-export `NextStudioHead` as default if you're happy with the default behavior
-export { NextStudioHead } from 'next-sanity/studio/head'
-
 export default function StudioPage() {
     //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
     return <NextStudio config={config} />
