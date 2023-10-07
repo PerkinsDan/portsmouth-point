209c7a4e9d314c72302f9603ad8a85f5561487df
Dan Perkins <danjonperkins@gmail.com>
2023-10-07 22:35:45 +0100

    fix: loading setup

--- app/(user)/results/loading.tsx
+++ app/(user)/results/loading.tsx
@@ -1,9 +1,9 @@
 import { Loader } from "@/components/Loader";
 
-export const loading = () => {
+export default function Loading() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <Loader />
         </div>
     );
-};
+}
