2b48db7ad55f91c96745972e70a87999efd006ca
Dan Perkins <danjonperkins@gmail.com>
2024-03-20 13:27:19 +0000

    fix: image not loading on some pages

--- components/studio/StudioButton.tsx
+++ components/studio/StudioButton.tsx
@@ -5,7 +5,7 @@ const StudioButton = () => {
         <div className="sticky flex justify-end w-full bottom-10">
             <a href="/studio" className="p-5 mr-10 border rounded-full">
                 <Image
-                    src="pencil.svg"
+                    src="/pencil.svg"
                     height={30}
                     width={30}
                     alt="pencil button"
