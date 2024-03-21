e2fa2e90a4672dec70f1ce146e6a90adb3e44922
Dan Perkins <danjonperkins@gmail.com>
2024-03-21 13:12:32 +0000

    feat: open in new window

--- components/studio/StudioButton.tsx
+++ components/studio/StudioButton.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 const StudioButton = () => {
     return (
         <div className="sticky flex justify-end w-full bottom-10">
-            <a href="/studio" className="p-5 mr-10 border rounded-full">
+            <a
+                href="/studio"
+                className="p-5 mr-10 border rounded-full"
+                target="_blank"
+            >
                 <Image
                     src="/pencil.svg"
                     height={30}
